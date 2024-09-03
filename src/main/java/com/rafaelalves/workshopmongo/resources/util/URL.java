ee528044eb18ca1807605726d44df4f4909212ae
Rafael540 <alves540@hotmail.com>
2024-09-03 15:03:48 -0300

    Consulta simples com query methods

---
+++ src/main/java/com/rafaelalves/workshopmongo/resources/util/URL.java
@@ -0,0 +1,15 @@
+package com.rafaelalves.workshopmongo.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+	
+	public static String decodeParam(String text) {
+		try {
+			return URLDecoder.decode(text, "UTD-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+}
--- src/main/java/com/rafaelalves/workshopmongo/services/PostService.java
+++ src/main/java/com/rafaelalves/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.rafaelalves.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +16,15 @@ public class PostService {
 	@Autowired
 	private PostRepository repo;
 
-
-
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
-	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 
+	
+	
 }
